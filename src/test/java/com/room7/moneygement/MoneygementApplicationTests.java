c80548fcc75c2ae31d41283cbc360e33a8282a70
HuiGyun-kim <khg940904@gmail.com>
2024-04-24 16:03:02 +0900

    Update: 기초 뼈대

---
+++ src/test/java/com/room7/moneygement/MoneygementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.room7.moneygement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneygementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
