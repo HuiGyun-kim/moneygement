3eb07377c4dd2f9aef5638f562b1639e0e78f177
euna kwon <eunakwon@naver.com>
2024-05-01 15:47:21 +0900

    style : 회원가입 아이콘을 svg파일로 교체함
    회원가입 때 사용하는 기업의 아이콘을 png에서 svg로 교체함
    
    Related to: #2

---
+++ src/main/resources/static/js/my-diary.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const dateButtonsContainer = document.getElementById('dateButtons');
+    const expenditureDetails = document.getElementById('expenditureDetails');
+
+    function generateDateButtons() {
+        const today = new Date();
+        const day = today.getDate();
+        const weeks = ['일', '월', '화', '수', '목', '금', '토'];
+
+        for (let i = -3; i <= 3; i++) {
+            const date = new Date(today);
+            date.setDate(day + i);
+            const week = date.getDay()
+            const button = document.createElement('button');
+            button.innerHTML = `${weeks[week]}<br>${date.getDate()}`;
+
+            if (date.getDay() === 6) { //토요일
+                button.style.color = 'blue';
+            }
+            else if (date.getDay() === 0) { //일요일
+                button.style.color = 'red';
+            }
+
+            if (i===0){ //여기서부터 작업하면 될 것 같음!!
+                button.style.border
+            }
+
+            button.onclick = function() {
+                showExpenditureDetails(date);
+            };
+            dateButtonsContainer.appendChild(button);
+        }
+    }
+
+    function showExpenditureDetails(date) {
+        expenditureDetails.textContent = `${date.getMonth() + 1}월 ${date.getDate()}일의 지출 내역`;
+    }
+
+    generateDateButtons();
+});
