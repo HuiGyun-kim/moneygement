894abec2e46136d49046c018f198e27d0fb7f0f5
euna kwon <eunakwon@naver.com>
2024-05-02 14:07:58 +0900

    Feat : 버튼과 연동하여 지출 내역이 보이게 함
    1. 버튼을 일부 수정하여 조금 더 UI 적으로 수정
    2. 버튼과 지출 내역을 연동하여, 각 버튼을 눌렀을 때 지출 내역이 바로 보일 수 있게 함.
    3. html, css를 수정하여, 아이콘과 일기 쓰기 버튼을 만듦.
    
    Related to: #9

--- src/main/resources/static/js/my-diary.js
+++ src/main/resources/static/js/my-diary.js
@@ -28,15 +28,58 @@ document.addEventListener('DOMContentLoaded', function() {
             }
 
             button.onclick = function() {
-                showExpenditureDetails(date);
+                showExpend(date);
             };
             dateButton.appendChild(button);
         }
     }
 
-    function showExpenditureDetails(date) {
-        expendsList.textContent = `${date.getMonth() + 1}월 ${date.getDate()}일의 지출 내역`;
+    function showExpend(date) {
+        const expendData = [
+            {date: new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1), time:'12:00', category: '식사', amount: '12000원', description: '점심 식사'},
+            {date: new Date(today.getFullYear(), today.getMonth(), today.getDate()), time:'13:00', category: '교통', amount: '3500원', description: '지하철 카드 충전'},
+            {date: new Date(today.getFullYear(), today.getMonth(), today.getDate()), time:'18:30', category: '쇼핑', amount: '59000원', description: '옷 구매'},
+            {date: new Date(2024, 4, 4), time:'13:00', category: '교통', amount: '3500원', description: '지하철 카드 충전'},
+            // monthidx는 0부터 시작해서 4.4해야 5월4일임
+            {date: new Date(2024, 4, 4), time:'18:30', category: '쇼핑', amount: '59000원', description: '옷 구매'}
+        ];
+
+        const filterData = expendData.filter(item =>
+            item.date.getFullYear() === date.getFullYear() &&
+            item.date.getMonth() === date.getMonth() &&
+            item.date.getDate() === date.getDate()
+        );
+
+        expendsList.innerHTML = '';
+
+        if (filterData.length === 0) {
+            expendsList.innerHTML = '해당 날짜에 대한 지출 내역이 없습니다.';
+            return;
+        }
+
+        const dateInfo = document.createElement('h3');
+        dateInfo.textContent = `${date.getMonth() + 1}월 ${date.getDate()}일의 지출 내역`;
+        expendsList.appendChild(dateInfo);
+
+        filterData.forEach(item => {
+            const time = document.createElement('div');
+            time.className = 'time';
+            time.textContent = item.time;
+            expendsList.appendChild(time);
+
+            const expendBox = document.createElement('div');
+            expendBox.className = 'expendBox';
+            expendBox.innerHTML = `
+            <p>${item.amount} ${item.description}</p>
+            `;
+
+            expendsList.appendChild(expendBox);
+        });
     }
 
+
+
     generateDateButtons();
+    const today = new Date();
+    showExpend(new Date(today.getFullYear(), today.getMonth(), today.getDate()));
 });
