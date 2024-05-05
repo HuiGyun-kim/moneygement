2cbc2cb8858cfb113fb56c65ea864b633b9556cb
euna kwon <eunakwon@naver.com>
2024-05-05 23:36:20 +0900

    Feat : 지출 내역 데이터베이스 연동을 위한 api 제작
    Related to: #33

--- src/main/java/com/room7/moneygement/controller/LedgerEntryController.java
+++ src/main/java/com/room7/moneygement/controller/LedgerEntryController.java
@@ -1,16 +1,35 @@
 package com.room7.moneygement.controller;
 
+import com.room7.moneygement.model.LedgerEntry;
+import com.room7.moneygement.repository.LedgerEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.room7.moneygement.service.LedgerEntryService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/ledgerEntry")
 public class LedgerEntryController {
 
-	private final LedgerEntryService ledgerEntryService;
+	@Autowired
+	private final LedgerEntryRepository ledgerEntryRepository;
+
+	@GetMapping("/expenses")
+	public List<LedgerEntry> getExpense(@RequestParam("date")
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date){
+		return ledgerEntryRepository.findByDate(date);
+	}
 }
