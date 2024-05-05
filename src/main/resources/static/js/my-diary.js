d54f5d2b868a8cc699e0f94f40f9cd19858b177a
euna kwon <eunakwon@naver.com>
2024-05-05 23:36:59 +0900

    Feat : 직접 데이터를 넣는 방식에서, 데이터베이스를 활용한 방식으로 변경함
    직접적으로 데이터를 넣어 구현한 전 방식과 다르게, 데이터베이스와 연동하여, 관리하고, 화면에 뿌릴 수 있도록 전부 코드를 수정함.
    
    Related to: #33

--- src/main/resources/static/js/my-diary.js
+++ src/main/resources/static/js/my-diary.js
@@ -71,10 +71,10 @@ document.addEventListener('DOMContentLoaded', function() {
             if (date.getFullYear() === today.getFullYear() &&
                 date.getMonth() === today.getMonth() &&
                 date.getDate() === today.getDate()){
-                button.style.borderColor = '#1A4F32';
-                button.style.borderWidth = '2px';
-                button.style.borderStyle = 'dashed';
-                button.style.borderRadius = '10px';
+                    button.style.borderColor = '#1A4F32';
+                    button.style.borderWidth = '2px';
+                    button.style.borderStyle = 'dashed';
+                    button.style.borderRadius = '10px';
             }
 
             button.onclick = function() {
@@ -84,38 +84,30 @@ document.addEventListener('DOMContentLoaded', function() {
             dateButton.appendChild(button);
         }
     }
-
     function showExpend(date) {
-        const expendData = [
-            {date: new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1), time:'12:00', category: '식사', amount: '12000원', description: '점심 식사'},
-            {date: new Date(today.getFullYear(), today.getMonth(), today.getDate()), time:'13:00', category: '교통', amount: '3500원', description: '지하철 카드 충전'},
-            {date: new Date(today.getFullYear(), today.getMonth(), today.getDate()), time:'18:30', category: '쇼핑', amount: '59000원', description: '옷 구매'},
-            {date: new Date(2024, 4, 4), time:'13:00', category: '교통', amount: '3500원', description: '점심먹기'},
-            // monthidx는 0부터 시작해서 4.4해야 5월4일임
-            {date: new Date(2024, 4, 4), time:'18:30', category: '쇼핑', amount: '59000원', description: '노래방가기'},
-            {date: new Date(2024, 4, 7), time:'18:30', category: '쇼핑', amount: '59000원', description: '노래방가기'}
-        ];
+        const dateString = date.toISOString().split('T')[0];
 
-        const filterData = expendData.filter(item =>
-            item.date.getFullYear() === date.getFullYear() &&
-            item.date.getMonth() === date.getMonth() &&
-            item.date.getDate() === date.getDate()
-        );
+        fetch(`/ledgerEntry/expenses?date=${dateString}`)
+        .then(response => response.json())
+        .then(data => {
+        displayExpenses(data, date);
+    })
+        .catch(error => console.error('에러:', error));
+    }
 
+    function displayExpenses(expensesData, date){
         expendsList.innerHTML = '';
+
+        if (expensesData.length === 0){
+        expendsList.innerHTML = '해당 날짜에 대한 지출 내역이 없습니다.';
+        return;
+        }
 
         const dateInfo = document.createElement('h4');
         dateInfo.textContent = `${date.getMonth() + 1}월 ${date.getDate()}일의 지출 내역`;
         expendsList.appendChild(dateInfo);
 
-
-        if (filterData.length === 0) {
-            expendsList.innerHTML += '해당 날짜에 대한 지출 내역이 없습니다.';
-            return;
-        }
-
-
-        filterData.forEach(item => {
+        expensesData.forEach(item => {
             const time = document.createElement('div');
             time.className = 'time';
             time.textContent = item.time;
@@ -124,11 +116,10 @@ document.addEventListener('DOMContentLoaded', function() {
             const expendBox = document.createElement('div');
             expendBox.className = 'expendBox';
             expendBox.innerHTML = `
-            <p>${item.amount} ${item.description}</p>
+            <p>${item.amount}원 ${item.description}</p>
             `;
 
             expendsList.appendChild(expendBox);
-
         });
 
         adjustHeight();
@@ -140,12 +131,77 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (lastExpendBox) {
             const dividerHeight = lastExpendBox.offsetTop + lastExpendBox.offsetHeight - timelineBox.offsetTop;
-            // 마지막 비용의 상단과 높이를 더해서 부모요소부터 박스까지의 상단 간격을 빼서 마지막 비용 항목의 아랫쪽 위치를 구할 수 있음.
+                // 마지막 비용의 상단과 높이를 더해서 부모요소부터 박스까지의 상단 간격을 빼서 마지막 비용 항목의 아랫쪽 위치를 구할 수 있음.
 
             const divider = document.querySelector('.deco-flex .divider');
             divider.style.height = `${dividerHeight}px`;
         }
     }
     generateDateButtons();
-    showExpend(new Date(today.getFullYear(), today.getMonth(), today.getDate()));
+    showExpend(new Date());
 });
+
+//     function showExpend(date) {
+//         const expendData = [
+//             {date: new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1), time:'12:00', category: '식사', amount: '12000원', description: '점심 식사'},
+//             {date: new Date(today.getFullYear(), today.getMonth(), today.getDate()), time:'13:00', category: '교통', amount: '3500원', description: '지하철 카드 충전'},
+//             {date: new Date(today.getFullYear(), today.getMonth(), today.getDate()), time:'18:30', category: '쇼핑', amount: '59000원', description: '옷 구매'},
+//             {date: new Date(2024, 4, 4), time:'13:00', category: '교통', amount: '3500원', description: '점심먹기'},
+//             // monthidx는 0부터 시작해서 4.4해야 5월4일임
+//             {date: new Date(2024, 4, 4), time:'18:30', category: '쇼핑', amount: '59000원', description: '노래방가기'},
+//             {date: new Date(2024, 4, 7), time:'18:30', category: '쇼핑', amount: '59000원', description: '노래방가기'}
+//         ];
+//
+//         const filterData = expendData.filter(item =>
+//             item.date.getFullYear() === date.getFullYear() &&
+//             item.date.getMonth() === date.getMonth() &&
+//             item.date.getDate() === date.getDate()
+//         );
+//
+//         expendsList.innerHTML = '';
+//
+//         const dateInfo = document.createElement('h4');
+//         dateInfo.textContent = `${date.getMonth() + 1}월 ${date.getDate()}일의 지출 내역`;
+//         expendsList.appendChild(dateInfo);
+//
+//
+//         if (filterData.length === 0) {
+//             expendsList.innerHTML += '해당 날짜에 대한 지출 내역이 없습니다.';
+//             return;
+//         }
+//
+//
+//         filterData.forEach(item => {
+//             const time = document.createElement('div');
+//             time.className = 'time';
+//             time.textContent = item.time;
+//             expendsList.appendChild(time);
+//
+//             const expendBox = document.createElement('div');
+//             expendBox.className = 'expendBox';
+//             expendBox.innerHTML = `
+//             <p>${item.amount} ${item.description}</p>
+//             `;
+//
+//             expendsList.appendChild(expendBox);
+//
+//         });
+//
+//         adjustHeight();
+//     }
+//
+//     function adjustHeight() {
+//         const timelineBox = document.querySelector('.timelineBox');
+//         const lastExpendBox = document.querySelector('.expendBox:last-child');
+//
+//         if (lastExpendBox) {
+//             const dividerHeight = lastExpendBox.offsetTop + lastExpendBox.offsetHeight - timelineBox.offsetTop;
+//             // 마지막 비용의 상단과 높이를 더해서 부모요소부터 박스까지의 상단 간격을 빼서 마지막 비용 항목의 아랫쪽 위치를 구할 수 있음.
+//
+//             const divider = document.querySelector('.deco-flex .divider');
+//             divider.style.height = `${dividerHeight}px`;
+//         }
+//     }
+//     generateDateButtons();
+//     showExpend(new Date(today.getFullYear(), today.getMonth(), today.getDate()));
+// });
