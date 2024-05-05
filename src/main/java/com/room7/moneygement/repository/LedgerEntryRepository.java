fe35cdb8581a2dac2a0a4e190537085def8a925a
euna kwon <eunakwon@naver.com>
2024-05-05 23:36:39 +0900

    Feat : 지출 내역 데이터베이스 연동을 위한 레포지토리 생성
    Related to: #33

--- src/main/java/com/room7/moneygement/repository/LedgerEntryRepository.java
+++ src/main/java/com/room7/moneygement/repository/LedgerEntryRepository.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Repository;
 
 import com.room7.moneygement.model.LedgerEntry;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface LedgerEntryRepository extends JpaRepository<LedgerEntry, Long> {
-
+    List<LedgerEntry> findByDate(LocalDate date);
 }
 
