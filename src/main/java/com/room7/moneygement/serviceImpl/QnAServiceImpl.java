ed6224a996b10651c290892fb60f310e8e424760
jinwooshin <sjw0851@gmail.com>
2024-05-07 13:20:07 +0900

    QnAContoller클래스, QnAService및 QnAServiceImpl클래스, 의존성 및 test코드 작성

--- src/main/java/com/room7/moneygement/serviceImpl/QnAServiceImpl.java
+++ src/main/java/com/room7/moneygement/serviceImpl/QnAServiceImpl.java
@@ -1,15 +1,49 @@
 package com.room7.moneygement.serviceImpl;
 
+import com.room7.moneygement.model.QnA;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.room7.moneygement.repository.QnARepository;
 import com.room7.moneygement.service.QnAService;
-
-import lombok.RequiredArgsConstructor;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-@RequiredArgsConstructor
 public class QnAServiceImpl implements QnAService {
-	private final QnARepository qnaRepository;
 
+	private final QnARepository qnARepository;
+
+	@Value("${alan.api.url}")
+	private String alanApiUrl;
+
+	@Value("${alan.api.key}")
+	private String alanApiKey;
+
+	public QnAServiceImpl(QnARepository qnARepository) {
+		this.qnARepository = qnARepository;
+	}
+
+	@Override
+	public String askQuestion(String question) {
+		// Alan Beta API를 호출하여 질문에 대한 답변을 받음
+		String answer = callAlanBetaApi(question);
+
+		// 질문한 목록과 대답을 데이터베이스에 저장
+		QnA qna = new QnA();
+		qna.setQuestion(question);
+		qna.setAnswer(answer);
+		qnARepository.save(qna);
+
+		return answer;
+	}
+
+	private String callAlanBetaApi(String question) {
+		RestTemplate restTemplate = new RestTemplate();
+		String url = alanApiUrl + "/ask"; // Alan Beta API 엔드포인트
+		String body = "{\"question\": \"" + question + "\"}";
+
+		// API 호출
+		String response = restTemplate.postForObject(url, body, String.class);
+		return response;
+	}
 }
