cd2c352a875461169e5ded0690c787846a7f4561
euna kwon <eunakwon@naver.com>
2024-05-09 22:18:36 +0900

    Docs: 챌린지에 관한 SecurityConfig 설정
    Related to: #45

--- src/main/java/com/room7/moneygement/config/SecurityConfig.java
+++ src/main/java/com/room7/moneygement/config/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig implements WebMvcConfigurer {
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/", "/login", "/signup", "/signup-email", "/users/**", "/ledgerEntry/**",
 								"/diary/**",
+								"/userChallenges/**","/attendance/**",
 								"/users/send-id-verification-code", "/users/verify-id-code", "/users/verifyEmail",
 								"/sendEmail",
 								"/users/sendEmail", "/emailVerified", "/find-id", "/users/find-id", "/find-password",
@@ -83,7 +84,7 @@ public class SecurityConfig implements WebMvcConfigurer {
 				.exceptionHandling(e -> e
 						.accessDeniedPage("/access-denied"))
 				.csrf(csrf -> csrf
-						.ignoringRequestMatchers("/ledgerEntry/**", "/users/sendEmail", "/diary/**")
+						.ignoringRequestMatchers("/ledgerEntry/**", "/users/sendEmail", "/diary/**", "/userChallenges/**")
 				);
 		return http.build();
 	}
