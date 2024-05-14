8dcac9cc3f3170e46195d8d376a2863946bdd62b
sohee <dno06127@naver.com>
2024-05-14 17:28:04 +0900

    design: 프로필 드롭메뉴 코드 수정
     #3

--- src/main/resources/static/js/dropdown.js
+++ src/main/resources/static/js/dropdown.js
@@ -13,3 +13,20 @@ document.querySelector('.more').addEventListener('click', function() {
     dropdownMenu.style.display = 'none';
 }
 });
+
+
+document.querySelector('.more2').addEventListener('click', function() {
+    var dropdownMenu = document.getElementById('myDropdown2');
+    if (dropdownMenu.style.display === 'block') {
+        dropdownMenu.style.display = 'none';
+    } else {
+        dropdownMenu.style.display = 'block';
+    }
+});
+// 드롭다운 메뉴말고 다른 공간 클릭하면 닫히도록 처리
+window.addEventListener('click', function(event) {
+    var dropdownMenu = document.getElementById('myDropdown2');
+    if (!event.target.matches('.more2')) {
+        dropdownMenu.style.display = 'none';
+    }
+});
