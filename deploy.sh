699fe2d4a6e000dfcb4a3e0df6d46f63081956cd
HuiGyun-kim <105576581+HuiGyun-kim@users.noreply.github.com>
2024-05-15 23:10:45 +0900

    Chore: 자동 배포 수정

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
-REPOSITORY=/home/ubuntu/deploy-fe # 배포된 프로젝트 경로.
 
-cd $REPOSITORY # 이 경로로 이동해서 밑에 명령어들을 차례로 실행.
+BUILD_JAR=$(ls /home/ec2-user/action/build/libs/*SNAPSHOT.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo "## build file name : $JAR_NAME" >> /home/ec2-user/action/spring-deploy.log
 
-sudo npm install --legacy-peer-deps # 의존성 파일 설치.
+echo "## copy build file" >> /home/ec2-user/action/spring-deploy.log
+DEPLOY_PATH=/home/ec2-user/action/
+cp $BUILD_JAR $DEPLOY_PATH
+
+echo "## current pid" >> /home/ec2-user/action/spring-deploy.log
+CURRENT_PID=$(ps -ef | grep "java" | awk 'NR==1 {print $2}')
+
+if [ -z $CURRENT_PID ]
+then
+  echo "## 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/spring-deploy.log
+else
+  echo "## kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "## deploy JAR file"   >> /home/ec2-user/action/spring-deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/action/spring-deploy.log 2> /home/ec2-user/action/spring-deploy_err.log &
