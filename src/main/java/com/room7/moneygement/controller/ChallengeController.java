1e18a5a9fd3a880f01080dff662e7996d1bc0575
euna kwon <eunakwon@naver.com>
2024-05-15 16:18:53 +0900

    Feat: 매달 1일마다 이달의 거지를 하나씩 개최할 수 있는 기능 추가
    Related to: #45

--- src/main/java/com/room7/moneygement/controller/ChallengeController.java
+++ src/main/java/com/room7/moneygement/controller/ChallengeController.java
@@ -1,10 +1,14 @@
 package com.room7.moneygement.controller;
 
+import com.room7.moneygement.model.Challenge;
+import com.room7.moneygement.repository.ChallengeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.room7.moneygement.service.ChallengeService;
-
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -13,4 +17,18 @@ import lombok.RequiredArgsConstructor;
 public class ChallengeController {
 
 	private final ChallengeService challengeService;
+	private final ChallengeRepository challengeRepository;
+
+	@Scheduled(cron = "0 0 0 1 * ?")
+	public void createNewChallenge() {
+		Challenge challenge = new Challenge();
+		challenge.setTitle("이달의 거지 챌린지");
+		challenge.setDescription("이달의 거지");
+		challenge.setReward("50");
+		challenge.setStartDate(LocalDate.now().withDayOfMonth(1).atStartOfDay());
+		challenge.setEndDate(LocalDate.now().withDayOfMonth(LocalDate.now().lengthOfMonth()).atStartOfDay());
+		challengeRepository.save(challenge);
+	}
+
+
 }
